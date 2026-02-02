94dd1cd33aa8cd5ffeec0e9f3190688c968dee56
santiago <santiagotoledosilvera@gmail.com>
2026-02-02 15:36:42 -0500

    ejercicio 1

---
+++ trabajo-python.py
@@ -0,0 +1,15 @@
+# Trabajo ejercicios en pareja
+
+print("""_____________Calculadora de intereses._____________
+Ingrese los valores para calcular los intereses  
+de un capital por la cantidad de años estimada.
+____________________________________________________
+""")
+
+def valores():
+    c = int(input("¿Cantidad del capital? "))
+    i = int(input("¿Porcentaje de intereses? "))
+    m = int(input("¿Tiempo estimado de el prestamo? "))
+    return (c * i) / 100 * m
+    
+print(f"{valores():.0f}")
